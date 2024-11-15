d3e63054efb434766f203bda7c104de8c95a18ae
rsattour <155961293+rsattour@users.noreply.github.com>
2024-11-15 22:45:54 +0100

    reda

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,36 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: risattou <risattou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ader <ader@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 17:26:47 by risattou          #+#    #+#             */
-/*   Updated: 2024/11/14 22:26:04 by risattou         ###   ########.fr       */
+/*   Created: 2021/10/19 11:15:27 by jdecorte          #+#    #+#             */
+/*   Updated: 2024/11/15 15:52:20 by ader             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <fcntl.h>
-# include <stdio.h>
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <limits.h>
 # include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
-
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
 
+
 char	*get_next_line(int fd);
-char	*ft_strdup(const char *src);
-size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_substr(char const *str, unsigned int start, size_t len);
-int		ft_strchr(const char *str, int c);
-char	*ft_help(char **all);
-size_t	ft_home(char **all, char *buff);
-char	*ft_zero(char **all, char **buff);
-char	*read_line(int fd, char **all, char *buff);
+char	*ft_strchr(const char *string, int searchedChar);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+
+size_t	ft_strlen(const char *theString);
 
 #endif
--- main.c
+++ main.c
@@ -1,48 +1,11 @@
-
-#include "get_next_line.h"
-#include <unistd.h>
-int main()
-{
-    int fd  = open("test.txt",O_RDWR);
-    printf("%s", get_next_line(fd));
-    printf("%s", get_next_line(fd));
-    printf("%s", get_next_line(fd));
-    // printf("%s", get_next_line(fd));
-    close(fd);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // int fd  =open("test.txt", O_CREAT | O_WRONLY);
-    // if (fd  == -1)
-    // {
-    //     printf("error");
-    //     return 0;
-    // }
-    // ft_putstr_fd("aderader",  fd);
-    // close(fd);
-    // if (fd  == -1)
-    // {
-    //     printf("error00");
-    //     return 0;
-    // }
-    // char *r = malloc(10);
-    // if(r == NULL)
-    // {
-    //     printf("error mal");
-    //     return 0;
-    // }
-    // int tet = read(fd,r,8);
-    // r[9] = '\0';
-    // close(fd);
-    // printf("$%s$\n %i",r,tet);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ader <ader@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:37:07 by ader              #+#    #+#             */
+/*   Updated: 2024/11/15 02:37:14 by ader             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
